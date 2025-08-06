6c3aa35805087c35ceeab47418df2fd3a98ccbba
julianatonu <julianatonu@gmail.com>
2025-08-06 21:48:21 +0600

    mobileMenu

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,18 +7,24 @@ import HeaderMenu from './HeaderMenu';
 import SearchBar from './SearchBar';
 import FavoriteButton from './FavoriteButton';
 import CartIcon from './CartIcon';
+import SignIn from './SignIn';
+import MobileMenu from './MobileMenu';
 
 const Header = () => {
 
     return (
         <header className='bg-white py-5 border-b border-black'>
             <Container className=' flex justify-between'>
-                <Logo />
+               <div className='w-auto md:w-1/3 flex items-center gap-3 justify-start md:gap-0'>
+                 <MobileMenu/>
+                 <Logo />
+               </div>
                 <HeaderMenu />
-                <div className='flex gap-2 w-auto md:1/3 justify-end items-center'>
+                <div className='flex gap-5 w-auto md:1/3 justify-end items-center'>
                     <SearchBar/>
                     <FavoriteButton/>
                     <CartIcon/>
+                    <SignIn/>
                 </div>
             </Container>
         </header>
