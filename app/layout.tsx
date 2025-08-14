b6a9b804a34f36ee910c45677cc50aa22705d871
julianatonu <julianatonu@gmail.com>
2025-08-14 11:30:06 +0600

    clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,34 @@
-"use client "
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 
-
-
 export const metadata: Metadata = {
   title: {
-template :"%s - Shopora online store",
-  default: "Shopora online store",
+    template: "%s - Shopora online store",
+    default: "Shopora online store",
   },
-  
   description: "Shopora online store",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <ClerkProvider>
-   <html lang="en">
+<html lang="en">
       <body className="font-poppins antialiased">
-        <Header/>
-        {children}
-        <Footer/>
+        
+          <Header />
+          {children}
+          <Footer />
+        
       </body>
     </html>
     </ClerkProvider>
- 
+    
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -9,13 +9,10 @@ import FavoriteButton from './FavoriteButton';
 import CartIcon from './CartIcon';
 import SignIn from './SignIn';
 import MobileMenu from './MobileMenu';
-import { currentUser } from '@clerk/nextjs/server';
-import { ClerkLoaded } from '@clerk/nextjs';
-import { SignedIn, UserButton } from '@clerk/clerk-react';
+
 
 const Header = async () => {
-const user = await currentUser()
-console.log("user",user)
+
     return (
         <header className='bg-white py-5 border-b border-black'>
             <Container className=' flex justify-between'>
@@ -27,13 +24,9 @@ console.log("user",user)
                 <div className='flex gap-5 w-auto md:1/3 justify-end items-center'>
                     <SearchBar/>
                     <CartIcon/>
-                    <FavoriteButton/>
-                    <ClerkLoaded>
-                        <SignedIn>
-                            <UserButton/>
-                        </SignedIn>
+                    <FavoriteButton/>                  
                     <SignIn/>
-                    </ClerkLoaded>
+                   
                 </div>
             </Container>
         </header>
